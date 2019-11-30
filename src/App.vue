789b1246b50e73702f5372f285274fb4fc8efe4a
fangshiqian <1143370870@qq.com>
2019-11-30 23:31:24 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
